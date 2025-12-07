abc3fd48d3fef763f5cb13faed3b3765a4ddd589
artyomdev250 <artyom.dev250@gmail.com>
2025-12-07 18:21:07 +0500

    GET products from /products

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Add from './pages/Add'
+import Edit from './pages/Edit'
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-[#F2F3F7] min-h-screen py-8 px-5">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/add' element={<Add />} />
+        <Route path='/edit' element={<Edit />} />
       </Routes>
     </div>
   )
